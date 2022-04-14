7b3b45042ba1ba8117c0d4d6b817d9175feb06f0
NazrinMammadova <102550859+NazrinMammadova@users.noreply.github.com>
2022-04-14 02:54:25 -0700

    added file

---
+++ task1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //// 1) Bir string arrayinin icersinde uzunlugu 5 den boyuk olan sozleri ekrana cixardan bir alqoritm yazin.
+
+
+
+            string[] words = { "Nazrin", "Firuza", "Gul", "Leyla" };
+            foreach (string word in words)
+            {
+                int wl = word.Length;
+                if (wl > 5)
+                {
+                    Console.WriteLine(word);
+                }
+
+            }
+
+
+        }
+    }
+}
+
